564274ba05ec7bda34477172dc7220252eef78b9
Kristof Cools <krcools@gmail.com>
2017-08-21 17:36:17 +0100

    Initial commit

--- src/LinearSpace.jl
+++ src/LinearSpace.jl
@@ -1,5 +1,30 @@
 module LinearSpace
 
-# package code goes here
+function lc_implementation(T::Type, F::Type{<:Number})
 
-end # module
+    LC = Symbol(T, :(_LC))
+    xp = quote
+        struct $(LC)
+            a::Vector{$F}
+            v::Vector{$T}
+        end
+
+        import Base: convert, promote_rule, +, *
+        convert(::Type{$LC}, u::$T) = $LC(ones($F,1),[u])
+        promote_rule(::Type{$T}, ::Type{$LC}) = $LC
+        Base.:+(x::Union{$LC,$T}, y::Union{$LC,$T}) = Base.:+(promote(x,y)...)
+        Base.:+(x::$LC, y::$LC) = $LC([x.a; y.a], [x.v; y.v])
+        Base.:*(a, x::$LC) = $LC(a*x.a, x.v)
+        Base.:*(a, x::$T) = a*convert($LC,x)
+    end
+
+    return xp
+end
+
+macro linearspace(T,F)
+    :(eval(lc_implementation($(esc(T)),$(esc(F)))))
+end
+
+export @linearspace
+
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,16 @@
 using LinearSpace
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+struct MT i::Int end
+
+@linearspace MT Float64
+x, y = MT(2), MT(3)
+
+z = x + 3y
+
+@test z.a == [1, 3]
+@test z.v == [x, y]
+
+z = 1x
+@test z.a == [1]
+@test z.v == [x]
